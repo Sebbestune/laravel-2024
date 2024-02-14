4313891eb706a0815c5b94d07351da5ae141044c
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-02-14 08:39:53 +0100

    Vercel stuff

--- api/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../app-31.1.2024-forms/public/index.php';
---
+++ app-31.1.2024-forms/api/index.php
@@ -0,0 +1,3 @@
+<?php
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../public/index.php';
